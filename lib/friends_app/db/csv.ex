6e1905135b4726e7ff22c1234eee39b4693413a5
Filipe Nogueira <nog.lipe@gmail.com>
2020-11-07 16:34:28 -0300

    Funcao delete criada

--- lib/friends_app/db/csv.ex
+++ lib/friends_app/db/csv.ex
@@ -4,24 +4,99 @@ defmodule FriendsApp.DB.CSV do
   alias FriendsApp.CLI.Friend
   alias NimbleCSV.RFC4180, as: CSVParser
 
-   def perform(chosen_menu_item) do
+  def perform(chosen_menu_item) do
     case chosen_menu_item do
       %Menu{label: _, id: :create} -> create()
       %Menu{id: :read, label: _} -> read()
       %Menu{id: :update, label: _} -> Shell.info("update")
-      %Menu{id: :delete, label: _} -> Shell.info("delete")
+      %Menu{id: :delete, label: _} -> delete()
     end
 
     FriendsApp.CLI.Menu.Choice.start()
   end
 
+  defp delete do
+    Shell.cmd("clear")
+
+    prompt_message("Digite o email do contato a ser deletado: ")
+    |> search_frind_by_email()
+    |> check_friend_found()
+    |> confirm_delete()
+    |> delete_and_save()
+  end
+
+  defp search_frind_by_email(email) do
+    get_struct_list_from_csv()
+    |> Enum.find(:not_found, fn list -> list.email == email end)
+  end
+
+  defp check_friend_found(friend) do
+    case friend do
+      :not_found ->
+        Shell.cmd("clear")
+        Shell.error("Amigo não Encontrado...")
+        Shell.prompt("Pressione ENTER para continuar")
+        FriendsApp.CLI.Menu.Choice.start()
+
+      _ ->
+        friend
+    end
+  end
+
+  defp confirm_delete(friend) do
+    Shell.cmd("clear")
+    Shell.info("Encontramos...")
+
+    show_friend(friend)
+
+    case Shell.yes?("Deseja realmente apagar esse amigo?") do
+      true -> friend
+      false -> :error
+    end
+  end
+
+  defp show_friend(friend) do
+    friend
+    |> Scribe.print(data: [{"Nome", :name}, {"Email", :email}, {"Telefone", :phone}])
+  end
+
+  defp delete_and_save(friend) do
+    case friend do
+      :error ->
+        Shell.info("Ok, amigo não será excluido...")
+        Shell.prompt("Pressione ENTER para continuar")
+
+      _ ->
+        get_struct_list_from_csv()
+        |> delete_friend_from_struct_list(friend)
+        |> friend_list_to_csv
+        |> prepare_list_to_save_csv
+        |> save_csv_file
+
+        Shell.info("Amigo Excluido com sucesso!")
+        Shell.prompt("Pressione ENTER para continuar")
+    end
+  end
+
+ defp delete_friend_from_struct_list(list, friend) do
+  list
+  |>Enum.reject(fn elem -> elem.email == friend.email end)
+ end
+
+ defp friend_list_to_csv(list) do
+  list
+  |>Enum.map(fn item ->
+    [item.email, item.name, item.phone]
+  end)
+ end
+
   defp read() do
-    get_struct_list_from_csv
+    get_struct_list_from_csv()
     |> show_friends
   end
 
   defp get_struct_list_from_csv do
-    read_csv_file
+    read_csv_file()
     |> parse_csv_file_to_list
     |> csv_list_to_friend_struct_list
   end
